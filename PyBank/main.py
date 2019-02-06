fa727e7537532792cf2e34a9127b10495d6c1997
Jason Dorman <jasondorman5@gmail.com>
2019-02-05 23:18:44 -0800

    first commit

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,57 @@
+import os
+import csv
 
+# Print title
+print('Financial Analysis')
+print('------------------------------')
+
+# direct path to budget data
+bugdetPath = os.path.join('budget_data.csv')
+
+# open budgetPath as read file
+with open(bugdetPath, newline='') as budgetFile:
+    # specify delimiter
+    budgetReader = csv.reader(budgetFile, delimiter=',')
+
+    # set counter to 0
+    revenue = 0
+    totalMonths = 0
+    monthlyChange = 0
+    previousrow = 0
+    counter = 0
+    max_increase = 0
+    ##max_increase_date = None
+    min_decrease = 0
+    #min_decrease_date = None
+
+    # skip header line when calculating profit/loss total
+    header = next(budgetReader)
+
+    # loop through rows to calculate profit/loss & month total
+    for row in budgetReader:
+        revenue += int(row[1])
+
+    # calculate total number of months
+        totalMonths += 1
+
+        if counter == 0:
+           lastrow = int(row[1])
+           counter +=1
+        else:
+            change = int(row[1])-lastrow
+            monthlyChange +=change
+            lastrow = int(row[1])
+
+            if max_increase ==0 or change < max_increase:
+                max_increase = change
+                #max_increase_date = date
+            if min_decrease ==0 or change < min_decrease:
+                min_decrease = change
+                #min_decrease_date = date
+    
+    # print
+    print('Total Months: ' , totalMonths)
+    print('Total: ' , '$' , revenue)
+    print('Average Change: ' , '$',round((monthlyChange / (totalMonths-1)),2))
+    print('Greatest Increase in Profits' , '$' , max_increase)
+    print('Greatest Decrease in Profits' , '$' , min_decrease)
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,59 @@
+import csv
+import os
 
+# Print title
+print('Election Results')
+print('------------------------------')
+
+# direct path to budget data
+electionPath = os.path.join('election_data.csv')
+
+# open budgetPath as read file
+with open(electionPath, newline='') as electionFile:
+    # specify delimiter
+    electionReader = csv.reader(electionFile, delimiter=',')
+
+    # set counter to 0
+    totalVotes = 0
+    countyTotal = 0
+    khan = 0
+    correy = 0
+    li = 0
+    otooley = 0
+
+    # set lists for columns
+    county = []
+    candidate = []
+
+    # skip header line when calculating profit/loss total
+    header = next(electionReader)
+
+    # loop through rows to calculate total # of votes
+    for row in electionReader:
+        totalVotes += len(row[1])
+        
+        # Khan totals
+        if row[1] == "Kahn":
+            khan+=1
+
+        # Correy totals
+        if totalVotes == "Correy":
+            correy+=1
+
+        # Li Totals
+        if totalVotes == "Li":
+            li+=1
+        
+        # O'Tooley Totals
+        if totalVotes == "O'Tooley":
+            otooley+=1
+        
+
+        khan_percent = (khan / totalVotes) * 100
+        correy_percent =(correy / totalVotes) * 100
+        li_percent = (li / totalVotes) * 100
+        otooley = (otooley / totalVotes) * 100
+
+    print('Total Votes: ' , totalVotes)
+    print('-------------------------------')
+    print('Khan : ' , khan_percent,'%')
